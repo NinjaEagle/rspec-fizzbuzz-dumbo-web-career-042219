cbc902ee56d2677e46bc2682baa1ce7890d9800b
NinjaEagle <kcwang06@gmail.com>
2019-04-01 00:33:05 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,14 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+def fizzbuzz (int)
+    if (int % 15 == 0)
+      console.log("FizzBuzz");
+    elseif (int % 3 == 0)
+      console.log("Fizz");
+    else if (int % 5 == 0)
+       console.log("Buzz");
+    else console.log(int);
+}
+
+
+end
